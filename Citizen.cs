4c73e273c48eebb7e834300feb8da719b8da8d0a
samar-mansour <52625367+samar-mansour@users.noreply.github.com>
2020-07-16 10:36:49 +0300

    Add files via upload

---
+++ Citizen.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw17_1207
+{
+    class Citizen
+    {
+        public string name;
+        public Citizen[] children = new Citizen[0];
+        public readonly int fatherID;
+        public readonly int id;
+        public static int numberOfCurrentCitizens = 0;
+        public const int MAXIMUM_NUMBER_OF_CITIZENS = 10000;
+
+        public static Random rnd = new Random();
+
+        public static int idCheak = RANDOM_NUMBER; // 1.auto-increment 2.Random
+        public const int AUTO_INCREMENT = 1; 
+        public const int RANDOM_NUMBER = 2;
+
+        public static bool[] idArray = new bool[MAXIMUM_NUMBER_OF_CITIZENS];
+
+        public Citizen(string name, int fatherID)
+        {
+            this.name = name;
+            this.fatherID = fatherID;
+
+            //everytime we create instanse we'll increasing citizin number
+            numberOfCurrentCitizens++;
+
+            switch (idCheak)
+            {
+                case AUTO_INCREMENT:
+                    this.id = numberOfCurrentCitizens;
+                    break;
+                case RANDOM_NUMBER:
+                    int newId = 0;
+                    do
+                    {
+                        newId = rnd.Next(MAXIMUM_NUMBER_OF_CITIZENS + 1);
+                    } while (idArray[newId] == true);
+                    idArray[newId] = true;
+                    this.id = newId;
+                    break;
+            }
+        }
+
+        public static void PrintNumberOfCitizens()
+        {
+            Console.WriteLine(Citizen.numberOfCurrentCitizens);
+        }
+
+        public static bool ReachedHalfOfMaximumSize()
+        {
+            if (Citizen.numberOfCurrentCitizens >= Citizen.MAXIMUM_NUMBER_OF_CITIZENS)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void PrintID()
+        {
+            Console.WriteLine(this.id);
+        }
+
+        public void PrintGapBetweenMyIDAndFather()
+        {
+            Console.WriteLine(Math.Abs(this.fatherID - this.id));
+        }
+
+        public void SetChildren(Citizen[] children)
+        {
+            this.children = children;
+        }
+
+        public override string ToString()
+        {
+            return $"[{base.ToString()}]: name: {this.name} Father ID: {this.fatherID} ID: {this.id}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw17_1207
+{
+    class Program
+    {
+        static bool HasChildren(Citizen citizen)
+        {
+            if (citizen.children.Length == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        static bool CheckValidity(Citizen citizen)
+        {
+            if (HasChildren(citizen))
+            {
+                for (int i = 0; i < citizen.children.Length; i++)
+                {
+                    if (citizen.id == citizen.children[i].fatherID)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            Citizen citizen1 = new Citizen("Mark", 1234);
+            citizen1.children = new Citizen[2] ;
+
+            Citizen citizen2 = new Citizen("Peter", 8596);
+            citizen2.children = new Citizen[0];
+
+            Citizen citizen3 = new Citizen("Karol", 4625);
+
+            Citizen.idCheak = Citizen.RANDOM_NUMBER;
+            Citizen[] child =
+            {
+                citizen1, citizen2, citizen3
+            };
+
+            citizen3.SetChildren(child);
+
+            Console.WriteLine(citizen1);
+            Console.WriteLine($"Has childrens? {HasChildren(citizen1)}");
+
+            Console.WriteLine(citizen2);
+            Console.WriteLine($"Has childrens? {HasChildren(citizen2)}");
+
+            Console.WriteLine(citizen3);
+            Console.WriteLine($"Has childrens? {HasChildren(citizen3)}");
+
+        }
+    }
+}
